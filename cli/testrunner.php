5a44d0b92733d709655f86779456e998c7c5ef7e
Imanuel Ulbricht <me@imanuel.dev>
2022-02-06 07:19:41 +0100

    Fix rights

---
+++ cli/testrunner.php
@@ -0,0 +1,9 @@
+<?php
+
+use League\Plates\Engine;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$eng = new Engine();
+$eng->addFolder('theme',__DIR__);
+echo $eng->render('theme::child');
